0e485782cb04e1ee62e88e41930388a408556030
luisFPA <luis.felipe.palomares.avena@students.iaac.net>
2024-01-25 14:43:14 -0600

    vue tests

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,98 @@ import { ref } from "vue" // you need this to use ref()
 
 
 
+
+
+
+
+
 <!-- the template is where the html code goes -->
 <template>
+<div id="navbar" class="container">  
+        <div id="title">Luis's website</div>
+        
+
+        <!--
+        <div id="logo">
+            <img src="cow.jpg" alt="macad cow">
+
+        </div>
+        -->
 
 
+    </div>
 
+    <div id="flex">
+
+        <div id="sidebar" class="container"> Sidebar </div>
+
+        <div id="main" class="container"> Text input </div>
+    </div>
 </template>
+
+
+
+
+
 
 
 
 <!-- style is where the css code goes -->
 <style scoped>
+html{
+    background-color: rgb(59, 59, 59); 
+    color: white;
+}
 
+body{
+    margin:0;
+}
+
+div{
+    /* put the borders in the inside of container */
+    box-sizing: border-box;
+}
+
+img{
+    height: 100%;
+    width: auto;
+}
+
+#navbar{
+    height: 50px;
+    border-color: red;
+}
+
+#flex{
+    display: flex;
+    height: calc(100vh - 50px);
+}
+
+#sidebar{
+    width:30%;
+    border-color: blue;
+}
+
+#main{
+    width:70%;
+    border-color: green;
+}
+
+#title{
+    width: 70%;
+    float:left;
+}
+
+#logo{
+    width: 30%;
+    height: 100%;
+    float:right;
+    text-align: right;
+}
+
+.container{
+    border-style: dotted;
+    border-width: 1px;
+}
 
 </style>
